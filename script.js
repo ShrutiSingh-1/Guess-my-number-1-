9805148755612912bd97307b16f19fb0bffc5f9b
Shruti Singh <98824644+ShrutiSingh-1@users.noreply.github.com>
2023-05-22 02:05:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
+
 'use strict';
-console.log(document.querySelector('.message').textContent);
+/*console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '😁 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+Document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);*/
+
+const secretNumber =  Math.trunc(Math.random()*20)+1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess =  document.querySelector('.guess').value;
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent ='⛔ No number!';
+    }else if (guess == secretNumber){
+      document.querySelector('.message').textContent = '😁 Correct Number!';
+    }else if (guess > secretNumber){
+      if(score > 1){
+      document.querySelector('.message').textContent = '👆 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }else{
+      document.querySelector('.message').textContent = '😫You lost the Game';  
+     }
+     } else if (guess < secretNumber){
+      if(score > 1){
+      document.querySelector('.message').textContent = '👇 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }else{
+      document.querySelector('.message').textContent = '😫You lost the Game';  
+    }
+  }
+  });
